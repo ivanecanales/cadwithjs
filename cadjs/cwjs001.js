17df60b00c9f2c33723a633368fcac134a79e1a1
Ivan Canales <Ivan.Canales@sqm.com>
2024-07-24 13:26:00 -0400

    Update older tasks

--- cadjs/cwjs001.js
+++ cadjs/cwjs001.js
@@ -12,101 +12,20 @@ const dwg = graphicElement(
     },
     document.body
 );
-const dimLines = [
-    [0, 20, 0, 80],
-    [20, 50, 20, 80],
-    [40, 70, 40, 80],
-    [60, 70, 60, 80],
-    [80, 50, 80, 80],
-    [0, 0, 0, -10],
-    [100, 0, 100, -10],
-    [100, 0, 110, 0],
-    [0, -8.5, 100, -8.5],
-    [100, 20, 110, 20],
-    [80, 50, 110, 50],
-    [60, 70, 78, 70],
-    [82, 70, 110, 70],
-    [0, 78.5, 80, 78.5],
-    [108.5, 0, 108.5, 70]
-];
-for(const dim of dimLines){
-    graphicElement(
-        'polyline',
-        {
-            'points': dim,
-            'class': 'dimline'
-        },
-        dwg
-    );
-};
-
-const arrow = {
-    'h': [0, 1.5, 3, 3, 3, 0, 0, 1.5],
-    'v': [0, 3, 3, 3, 1.5, 0, 0, 3]
-};
-const arrowLoc = [
-    {'x': 1, 'y': 1, 'c': [0, 77], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [20, 77], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [40, 77], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [60, 77], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [100, -10], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [20, 77], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [40, 77], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [60, 77], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [80, 77], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [0, -10], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [107, 0], 'd': 'v'},
-    {'x': 1, 'y': 1, 'c': [107, 20], 'd': 'v'},
-    {'x': 1, 'y': 1, 'c': [107, 50], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 20], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 50], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 70], 'd': 'v'}
-];
-for(const al of arrowLoc){
-    graphicElement(
-        'polyline',
-        {
-            'points': arrow[al['d']],
-            'class': 'dimarrow',
-            'transform': `matrix(${[al['x'], 0, 0, al['y'], ...al['c']]})`
-        },
-        dwg
-    );
-};
-const d = 2.5;
-const dimensions = [
-    {'l': 2, 'o': 'h', 'c': [10, 80]},
-    {'l': 2, 'o': 'h', 'c': [30, 80]},
-    {'l': 2, 'o': 'h', 'c': [50, 80]},
-    {'l': 2, 'o': 'h', 'c': [70, 80]},
-    {'l': 2, 'o': 'v', 'c': [110, 10]},
-    {'l': 5, 'o': 'v', 'c': [110, 35]},
-    {'l': 2, 'o': 'v', 'c': [110, 60]},
-    {'l': 10, 'o': 'h', 'c': [50, -13.5]}
-];
-for(const dim of dimensions){
-    const rect = d * dim['l'].toString().length;
-    const x = dim['o'] == 'h' ? dim['c'][0] - 0.5 * rect : dim['c'][0];
-    const y = dim['o'] == 'h' ? dim['c'][1] : dim['c'][1] - 0.5 * rect + d;
-    const m = dim['o'] == 'h' ? [1, 0, 0, -1] : [0, -1, -1, 0];
-    graphicElement(
-        'text',
-        {
-            'font-family': 'Ubuntu Mono',
-            'font-size': 5,
-            'transform': `matrix(${[...m, x, y]})`,
-            'textLength': rect,
-            'lengthAdjust': 'spacingAndGlyphs',
-            'fill': 'teal'
-        },
-        dwg,
-        dim['l']
-    );
-};
+const points = [0, 0, 100, 0, 100, 20, 80, 20, 80, 50, 60, 50, 60, 70, 40, 70, 40, 50, 20, 50, 20, 20, 0, 20, 0, 0];
+const part = pointsToCoords(points);
+drawDim(part[0], part[1], 'h', 'b', 10, 10, dwg);
+drawDim(part[1], part[2], 'v', 'r', 10, 2, dwg);
+drawDim(part[3], part[4], 'v', 'r', 30, 3, dwg);
+drawDim(part[5], part[6], 'v', 'r', 50, 2, dwg);
+drawDim(part[4], part[5], 'h', 'u', 30, 2, dwg);
+drawDim(part[6], part[7], 'h', 'u', 10, 2, dwg);
+drawDim(part[8], part[9], 'h', 'u', 30, 2, dwg);
+drawDim(part[10], part[11], 'h', 'u', 60, 2, dwg);
 graphicElement(
     'polyline',
     {
-        'points': [0, 0, 100, 0, 100, 20, 80, 20, 80, 50, 60, 50, 60, 70, 40, 70, 40, 50, 20, 50, 20, 20, 0, 20, 0, 0],
+        'points': points,
         'class': 'part'
     },
     dwg
--- cadjs/cwjs002.js
+++ cadjs/cwjs002.js
@@ -12,99 +12,16 @@ const dwg = graphicElement(
     },
     document.body
 );
-const dimLines = [
-    [0, 100, 0, 110],
-    [20, 100, 20, 110],
-    [40, 80, 40, 110],
-    [80, 40, 80, 110],
-    [0, 0, 0, -10],
-    [100, 0, 100, -10],
-    [100, 0, 110, 0],
-    [100, 20, 110, 20],
-    [80, 40, 110, 40],
-    [40, 80, 78.5, 80],
-    [81.5, 80, 110, 80],
-    [20, 100, 38.5, 100],
-    [41.5, 100, 78.5, 100],
-    [81.5, 100, 110, 100],
-    [0, 108.5, 80, 108.5],
-    [108.5, 0, 108.5, 100],
-    [0, -8.5, 100, -8.5]
-];
-for(const dim of dimLines){
-    graphicElement(
-        'polyline',
-        {
-            'points': dim,
-            'class': 'dimline'
-        },
-        dwg
-    );
-};
-
-const arrow = {
-    'h': [0, 1.5, 3, 3, 3, 0, 0, 1.5],
-    'v': [0, 3, 3, 3, 1.5, 0, 0, 3]
-};
-const arrowLoc = [
-    {'x': 1, 'y': 1, 'c': [0, 107], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [20, 107], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [40, 107], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [0, -10], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [20, 107], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [40, 107], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [80, 107], 'd': 'h'},
-    {'x': -1, 'y': 1, 'c': [100, -10], 'd': 'h'},
-    {'x': 1, 'y': 1, 'c': [107, 0], 'd': 'v'},
-    {'x': 1, 'y': 1, 'c': [107, 20], 'd': 'v'},
-    {'x': 1, 'y': 1, 'c': [107, 40], 'd': 'v'},
-    {'x': 1, 'y': 1, 'c': [107, 80], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 20], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 40], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 80], 'd': 'v'},
-    {'x': 1, 'y': -1, 'c': [107, 100], 'd': 'v'}
-];
-for(const al of arrowLoc){
-    graphicElement(
-        'polyline',
-        {
-            'points': arrow[al['d']],
-            'class': 'dimarrow',
-            'transform': `matrix(${[al['x'], 0, 0, al['y'], ...al['c']]})`
-        },
-        dwg
-    );
-};
-const d = 2.5;
-const dimensions = [
-    {'l': 2, 'o': 'h', 'c': [10, 110]},
-    {'l': 2, 'o': 'h', 'c': [30, 110]},
-    {'l': 4, 'o': 'h', 'c': [60, 110]},
-    {'l': 10, 'o': 'h', 'c': [50, -13.5]},
-    {'l': 2, 'o': 'v', 'c': [110, 10]},
-    {'l': 2, 'o': 'v', 'c': [110, 30]},
-    {'l': 4, 'o': 'v', 'c': [110, 60]},
-    {'l': 2, 'o': 'v', 'c': [110, 90]}
-];
-for(const dim of dimensions){
-    const rect = 2.5 * dim['l'].toString().length;
-    const x = dim['o'] == 'h' ? dim['c'][0] - 0.5 * rect : dim['c'][0];
-    const y = dim['o'] == 'h' ? dim['c'][1] : dim['c'][1] - 0.5 * rect + d;
-    const m = dim['o'] == 'h' ? [1, 0, 0, -1] : [0, -1, -1, 0];
-    graphicElement(
-        'text',
-        {
-            'font-family': 'Ubuntu Mono',
-            'font-size': 5,
-            'transform': `matrix(${[...m, x, y]})`,
-            'textLength': rect,
-            'lengthAdjust': 'spacingAndGlyphs',
-            'fill': 'teal'
-        },
-        dwg,
-        dim['l']
-    );
-};
+const points = [0, 0, 100, 0, 100, 20, 80, 20, 80, 40, 40, 40, 40, 80, 20, 80, 20, 100, 0, 100, 0, 0];
+const part = pointsToCoords(points);
+drawDim(part[0], part[1], 'h', 'b', 10, 10, dwg);
+drawDim(part[1], part[2], 'v', 'r', 10, 2, dwg);
+drawDim(part[3], part[4], 'v', 'r', 30, 2, dwg);
+drawDim(part[5], part[6], 'v', 'r', 70, 4, dwg);
+drawDim(part[7], part[8], 'v', 'r', 90, 2, dwg);
+drawDim(part[4], part[5], 'h', 'u', 70, 4, dwg);
+drawDim(part[6], part[7], 'h', 'u', 30, 2, dwg);
+drawDim(part[8], part[9], 'h', 'u', 10, 2, dwg);
 graphicElement(
     'polyline',
     {
